4fa0885fb23a7f82413e075c64d8927b9b7a55d1
ryolingo <ryomatsuya04@gmail.com>
2024-08-05 18:22:21 +0900

    get api 作成

---
+++ app/api/blog/route.ts
@@ -0,0 +1,24 @@
+import { PrismaClient } from "@prisma/client";
+import { NextResponse } from "next/server";
+
+const prisma = new PrismaClient();
+
+export async function main() {
+  try {
+    await prisma.$connect();
+  } catch (err) {
+    return "DB接続に失敗しました";
+  }
+}
+
+export const GET = async (req: Request, res: NextResponse) => {
+  try {
+    await main();
+    const posts = await prisma.feedback.findMany();
+    return NextResponse.json({ message: "Success", posts }, { status: 200 });
+  } catch (err) {
+    return NextResponse.json({ message: "Error", err }, { status: 500 });
+  } finally {
+    await prisma.$disconnect();
+  }
+};
