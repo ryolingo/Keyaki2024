33db553c517e069be745d17e0840adb52a5fd503
ryolingo <ryomatsuya04@gmail.com>
2024-08-05 23:33:01 +0900

    post APIの作成

--- app/api/feedback/route.ts
+++ app/api/feedback/route.ts
@@ -10,12 +10,26 @@ export async function main() {
     return "DB接続に失敗しました";
   }
 }
-
+//全FEEDBACK取得用API
 export const GET = async (req: Request, res: NextResponse) => {
   try {
     await main();
     const feedback = await prisma.feedback.findMany();
     return NextResponse.json({ message: "Success", feedback }, { status: 200 });
+  } catch (err) {
+    return NextResponse.json({ message: "Error", err }, { status: 500 });
+  } finally {
+    await prisma.$disconnect();
+  }
+};
+
+//FEEDBACk投稿用API
+export const POST = async (req: Request, res: NextResponse) => {
+  try {
+    const { comment } = await req.json();
+    await main();
+    const feedback = await prisma.feedback.create({ data: { comment } });
+    return NextResponse.json({ message: "Success", feedback }, { status: 201 });
   } catch (err) {
     return NextResponse.json({ message: "Error", err }, { status: 500 });
   } finally {
