55f59e1e768f259e686810dae42eaf05e0bee219
ryolingo <ryomatsuya04@gmail.com>
2024-08-16 13:56:32 +0900

    MUIによるUI調整

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -1,33 +1,54 @@
 import Link from "next/link";
 import fetchALLFeedbacks from "../hooks/useFeedback";
 import { PostType } from "../types";
+import { Card, CardContent, Container, Typography } from "@mui/material";
 
 export default async function Home() {
   const feedbacks = await fetchALLFeedbacks();
   return (
-    <main>
-      <div>
+    <Container>
+      <Typography>
         <h1>欅祭アンケート</h1>
-      </div>
+      </Typography>
       {/* Link */}
-      <div>
-        <Link href={"/feedback"}>ブログ新規作成</Link>
-      </div>
 
-      <div>
+      <Link href={"/feedback"}>
+        <Typography>ブログ新規作成</Typography>
+      </Link>
+
+      <Typography>
         {feedbacks?.map((feedback: PostType) => (
-          <div key={feedback.id}>
-            <div>
-              <h2>{feedback.comment}</h2>
-            </div>
-            <div>
-              <blockquote>
-                {new Date(feedback.createdAt).toLocaleString()}
-              </blockquote>
-            </div>
-          </div>
+          <Typography style={{ marginBottom: "60px" }} key={feedback.id}>
+            <Card
+              sx={{
+                border: "2px solid darkgray ",
+                boxShadow: "none",
+                borderRadius: "8px",
+                "&:hover": {
+                  boxShadow: "0px 4px 20px rgba(0,0,0,0.15)",
+                },
+              }}
+            >
+              <CardContent>
+                <Typography
+                  variant="h6"
+                  component="h2"
+                  sx={{
+                    paddingLeft: "0px",
+                    textAlign: "left",
+                    fontWeight: "bold",
+                  }}
+                >
+                  {feedback.comment}
+                </Typography>
+                <Typography fontSize="13px" sx={{ paddingLeft: "0" }}>
+                  {new Date(feedback.createdAt).toLocaleString()}
+                </Typography>
+              </CardContent>
+            </Card>
+          </Typography>
         ))}
-      </div>
-    </main>
+      </Typography>
+    </Container>
   );
 }
