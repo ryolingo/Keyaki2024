b7aefda04b6c51a19cf61488b895a6dbc90a6856
ryolingo <ryomatsuya04@gmail.com>
2024-08-17 15:55:18 +0900

    GetのUI表示

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -1,54 +1,48 @@
 import Link from "next/link";
 import fetchALLFeedbacks from "../hooks/useFeedback";
 import { PostType } from "../types";
-import { Card, CardContent, Container, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  Container,
+  Typography,
+} from "@mui/material";
 
 export default async function Home() {
   const feedbacks = await fetchALLFeedbacks();
   return (
     <Container>
-      <Typography>
-        <h1>欅祭アンケート</h1>
+      <Typography fontSize={80} display="flex" justifyContent="center">
+        欅祭アンケート
       </Typography>
       {/* Link */}
 
-      <Link href={"/feedback"}>
-        <Typography>ブログ新規作成</Typography>
-      </Link>
-
-      <Typography>
+      <div>
         {feedbacks?.map((feedback: PostType) => (
-          <Typography style={{ marginBottom: "60px" }} key={feedback.id}>
-            <Card
-              sx={{
-                border: "2px solid darkgray ",
-                boxShadow: "none",
-                borderRadius: "8px",
-                "&:hover": {
-                  boxShadow: "0px 4px 20px rgba(0,0,0,0.15)",
-                },
-              }}
+          // <FeedBackCard comment={feedback.comment} createdAt={feedback.cre} />
+          <Box
+            border={1}
+            borderColor="gainsboro"
+            padding={3}
+            marginBottom={2}
+            key={feedback.id}
+          >
+            <Typography
+              component="h2"
+              paddingLeft="0px"
+              textAlign="left"
+              fontSize="13px"
             >
-              <CardContent>
-                <Typography
-                  variant="h6"
-                  component="h2"
-                  sx={{
-                    paddingLeft: "0px",
-                    textAlign: "left",
-                    fontWeight: "bold",
-                  }}
-                >
-                  {feedback.comment}
-                </Typography>
-                <Typography fontSize="13px" sx={{ paddingLeft: "0" }}>
-                  {new Date(feedback.createdAt).toLocaleString()}
-                </Typography>
-              </CardContent>
-            </Card>
-          </Typography>
+              {feedback.comment}
+            </Typography>
+            <Typography fontSize="10px">
+              {new Date(feedback.createdAt).toLocaleString()}
+            </Typography>
+          </Box>
         ))}
-      </Typography>
+      </div>
     </Container>
   );
 }
