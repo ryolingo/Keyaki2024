f19d80490c84bcdfba47eacaf3aeb64278de4836
ryolingo <ryomatsuya04@gmail.com>
2024-09-24 15:37:08 +0900

    不要なコード削除

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,7 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import React from "react";
 import Header from "./components/Header";
 import ToastProvider from "./components/ToastProvider";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
