555127103506fcd2842036e9d3f9be3c1d87bd40
ryolingo <ryomatsuya04@gmail.com>
2024-09-24 16:38:21 +0900

    consoleてスト

--- app/components/HomeContainer.tsx
+++ app/components/HomeContainer.tsx
@@ -3,6 +3,7 @@ import getFeedback from "../actions/getFeedback";
 
 const HomeContainer = async () => {
   const feedbacks = await getFeedback();
+  console.log(feedbacks);
   if (!feedbacks) return <div>loading...</div>;
 
   return <Home feedbacks={feedbacks} />;
