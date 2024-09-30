e54a9295d599898d1d70aaa76d89ac0d5d0ca10f
Ryota matsumoto <ryomatsuya04@gmail.com>
2024-09-30 16:59:22 +0900

    Revert "Fix/usefeedback"

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
+import getFeedback from "./actions/getFeedback";
 import Home from "./components/Home";
-import fetchALLFeedbacks from "./hooks/useFeedback";
 
 const HomeContainer = async () => {
-  const feedbacks = await fetchALLFeedbacks();
+  const feedbacks = await getFeedback();
   console.log(feedbacks);
   if (!feedbacks) return <div>loading...</div>;
 
