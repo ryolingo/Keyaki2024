af25960ba19c07de47fb82ddee80676fed3c2f92
ryolingo <ryomatsuya04@gmail.com>
2024-10-08 00:59:16 +0900

    getfeedbackに変更

--- app/hooks/useFeedback.ts
+++ app/hooks/useFeedback.ts
@@ -1,23 +1,21 @@
-import axios from "axios";
-
 const apiUrl = process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3000";
 
 export async function fetchALLFeedbacks() {
-  const response = await axios.request({
-    url: `${apiUrl}/api/feedback`,
-    method: "GET",
+  const res = await fetch(`${apiUrl}/api/feedback`, {
+    cache: "no-store",
   });
-  return response.data.feedbacks;
+  const data = await res.json();
+  return data.feedbacks;
 }
 
-// export async function postFeedback(comment: string) {
-//   const res = await fetch(`${apiUrl}/api/feedback`, {
-//     method: "POST",
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//     body: JSON.stringify({ comment, createdAt: new Date() }),
-//   });
+// // export async function postFeedback(comment: string) {
+// //   const res = await fetch(`${apiUrl}/api/feedback`, {
+// //     method: "POST",
+// //     headers: {
+// //       "Content-Type": "application/json",
+// //     },
+// //     body: JSON.stringify({ comment, createdAt: new Date() }),
+// //   });
 
-//   return res.json();
-// }
+// //   return res.json();
+// // }
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
+import getFeedback from "./actions/getFeedback";
 import Home from "./components/Home";
 import { fetchALLFeedbacks } from "./hooks/useFeedback";
 
 const page = async () => {
-  const feedbacks = await fetchALLFeedbacks();
+  const feedbacks = await getFeedback();
   console.log(feedbacks);
   if (!feedbacks) return <div>loading...</div>;
 
